2576394f0eb2b78c31fce6c17d0570827e3ae8a1
nophakhun <nophakhun@superboy.jp>
2024-04-12 17:31:03 +0900

    komikomi

--- oppai.ts
+++ oppai.ts
@@ -1,7 +1,22 @@
-let oppai = [6]
+let oppai = [6];
 
-oppai[0] += 5
+oppai[0] += 5;
 
-oppai[1] += 8
+oppai[1] += 8;
 
-oppai.map
+oppai.map;
+
+const aaa = 10;
+const aab = 10;
+const aac = 10;
+const aad = 10;
+const aae = 10;
+const aaf = 10;
+const aag = 10;
+const aah = 10;
+const aai = 10;
+const aaj = 10;
+const aak = 10;
+const aal = 10;
+const aaam = 10;
+ß;
